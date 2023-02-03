d46e66a2d9712decd38732ec619205edb12ad988
Kunal Khairnar <95234444+Kunal-Khairnar-05@users.noreply.github.com>
2023-02-03 15:46:29 +0530

    Add : Practical 1

---
+++ Practical_1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string message = "Hello, World!";
+
+  cout << endl;
+
+  int n = message.length();
+  for (int i = 0; i < n + 4; i++) {
+    cout << "*";
+  }
+  cout << endl;
+  cout << "* " << message << " *" << endl;
+  for (int i = 0; i < n + 4; i++) {
+    cout << "*";
+  }
+  cout << endl << endl;
+
+  return 0;
+}
